5eea7bcc93339b84fdb67d812f9937d724f20cf8
Tabib Imran <in.with.imran@gmail.com>
2026-08-11 23:33:48 +0600

    Add files via upload

--- Data/musicData.js
+++ Data/musicData.js
@@ -1,1 +1,58 @@
-
+window.PLAYLIST_DATA = [
+            {
+                id: 'track-1',
+                title: 'Baby',
+                artist: 'Justin Bieber',
+                audioUrl: 'https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3?filename=lofi-study-112191.mp3',
+                searchQuery: ''
+            },
+            {
+                id: 'track-2',
+                title: 'Let Me Down Slowly',
+                artist: 'Alec Benjamin',
+                audioUrl: 'https://www.bensound.com/bensound-music/bensound-acousticbreeze.mp3',
+                searchQuery: 'Acoustic Breeze Benjamin Tissot'
+            },
+            {
+                id: 'track-3',
+                title: 'Creative Minds',
+                artist: 'Bensound',
+                audioUrl: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3',
+                searchQuery: 'Creative Minds Bensound'
+            },
+        {
+                id: 'track-4',
+                title: 'Creative Minds',
+                artist: 'Bensound',
+                audioUrl: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3',
+                searchQuery: 'Creative Minds Bensound'
+            },
+        {
+                id: 'track-5',
+                title: 'Creative Minds',
+                artist: 'Bensound',
+                audioUrl: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3',
+                searchQuery: 'Creative Minds Bensound'
+            },
+        {
+                id: 'track-6',
+                title: 'Creative Minds',
+                artist: 'Bensound',
+                audioUrl: 'https://www.bensound.com/bensound-music/bensound-creativeminds.mp3',
+                searchQuery: 'Creative Minds Bensound'
+            },
+            {
+                id: 'track-4',
+                title: 'Memories',
+                artist: 'Bensound',
+                audioUrl: 'https://www.bensound.com/bensound-music/bensound-memories.mp3',
+                searchQuery: 'Memories Bensound'
+            },
+            {
+                id: 'track-5',
+                title: 'Ukulele Joy',
+                artist: 'Bensound',
+                audioUrl: 'https://www.bensound.com/bensound-music/bensound-ukulele.mp3',
+                searchQuery: 'Ukulele Bensound'
+            }
+        ];

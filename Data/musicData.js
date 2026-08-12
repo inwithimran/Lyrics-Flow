96a6c40cfc79a01ef06852689e91e59ab3543678
Tabib Imran <in.with.imran@gmail.com>
2026-08-13 00:47:00 +0600

    Refactor music data structure and add new track

--- Data/musicData.js
+++ Data/musicData.js
@@ -3,56 +3,54 @@ window.PLAYLIST_DATA = [
                 id: 'track-1',
                 title: 'Meri Banogi Kya',
                 artist: 'Rito Riba',
-                audioUrl: 'Data/music/Rito Riba - Meri Banogi Kya.mp3',
-                searchQuery: 'Meri Banogi Kya Rito Riba'
+                audioUrl: 'Data/music/Rito Riba - Meri Banogi Kya.mp3'
             },
             {
                 id: 'track-2',
                 title: 'Let Her Go',
                 artist: 'Passenger',
-                audioUrl: 'Data/music/Passenger - Let Her Go.mp3',
-                searchQuery: 'Passenger Let Her Go'
+                audioUrl: 'Data/music/Passenger - Let Her Go.mp3'
             },
             {
                 id: 'track-3',
                 title: 'Juliet',
                 artist: 'Cavetown',
-                audioUrl: 'Data/music/Cavetown - Juliet.mp3',
-                searchQuery: 'Cavetown Juliet'
+                audioUrl: 'Data/music/Cavetown - Juliet.mp3'
             },
         {
                 id: 'track-4',
                 title: 'Somewhere Only We Know',
                 artist: 'Keane',
-                audioUrl: 'Data/music/Keane - Somewhere Only We Know.mp3',
-                searchQuery: 'Keane Somewhere Only We Know'
+                audioUrl: 'Data/music/Keane - Somewhere Only We Know.mp3'
             },
         {
                 id: 'track-5',
                 title: 'Attention',
                 artist: 'Charlie Puth',
-                audioUrl: 'Data/music/Charlie Puth - Attention.mp3',
-                searchQuery: 'Charlie Puth Attention'
+                audioUrl: 'Data/music/Charlie Puth - Attention.mp3'
             },
         {
                 id: 'track-6',
-                title: 'Mera Pyar Tera Pyar.mp3',
+                title: 'Mera Pyar Tera Pyar',
                 artist: 'Jeet Gannguli',
-                audioUrl: 'Data/music/Jeet Gannguli - Mera Pyar Tera Pyar.mp3',
-                searchQuery: 'Jeet Gannguli Mera Pyar Tera Pyar'
+                audioUrl: 'Data/music/Jeet Gannguli - Mera Pyar Tera Pyar.mp3'
             },
             {
                 id: 'track-7',
                 title: 'Dai Dai',
                 artist: 'Shakira',
-                audioUrl: 'Data/music/Shakira - Dai Dai.mp3',
-                searchQuery: 'Memories Bensound'
+                audioUrl: 'Data/music/Shakira - Dai Dai.mp3'
             },
             {
                 id: 'track-8',
                 title: 'Goals',
                 artist: 'LISA',
-                audioUrl: 'Data/music/LISA - Goals.mp3',
-                searchQuery: 'Ukulele Bensound'
+                audioUrl: 'Data/music/LISA - Goals.mp3'
+            },
+            {
+                id: 'track-9',
+                title: 'Criminal',
+                artist: 'Britney Spears',
+                audioUrl: 'Data/music/Britney Spears - Criminal.mp3'
             }
         ];

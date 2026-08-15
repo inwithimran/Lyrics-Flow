a6b6c9077b5d856da2a85efafcf59fbf95028a63
Tabib Imran <in.with.imran@gmail.com>
2026-08-15 20:27:12 +0600

    Add coverUrl for music tracks in musicData.js

--- Data/musicData.js
+++ Data/musicData.js
@@ -4,25 +4,28 @@ window.PLAYLIST_DATA = [
                 title: 'Meri Banogi Kya',
                 artist: 'Rito Riba',
                 audioUrl: 'Data/music/Rito Riba - Meri Banogi Kya.mp3',
-                
+                coverUrl: 'Data/covers/1.jpg'
             },
             {
                 id: 'track-2',
                 title: 'Let Her Go',
                 artist: 'Passenger',
-                audioUrl: 'Data/music/Passenger - Let Her Go.mp3'
+                audioUrl: 'Data/music/Passenger - Let Her Go.mp3',
+                coverUrl: 'Data/covers/2.jpg'
             },
             {
                 id: 'track-3',
                 title: 'Juliet',
                 artist: 'Cavetown',
-                audioUrl: 'Data/music/Cavetown - Juliet.mp3'
+                audioUrl: 'Data/music/Cavetown - Juliet.mp3',
+                coverUrl: 'Data/covers/3.jpg'
             },
         {
                 id: 'track-4',
                 title: 'Somewhere Only We Know',
                 artist: 'Keane',
-                audioUrl: 'Data/music/Keane - Somewhere Only We Know.mp3'
+                audioUrl: 'Data/music/Keane - Somewhere Only We Know.mp3',
+                coverUrl: 'Data/covers/4.jpg'
             },
         {
                 id: 'track-5',
@@ -35,24 +38,36 @@ window.PLAYLIST_DATA = [
                 id: 'track-6',
                 title: 'Mera Pyar Tera Pyar',
                 artist: 'Jeet Gannguli',
-                audioUrl: 'Data/music/Jeet Gannguli - Mera Pyar Tera Pyar.mp3'
+                audioUrl: 'Data/music/Jeet Gannguli - Mera Pyar Tera Pyar.mp3',
+                coverUrl: 'Data/covers/6.jpg'
             },
             {
                 id: 'track-7',
                 title: 'Dai Dai',
                 artist: 'Shakira',
-                audioUrl: 'Data/music/Shakira - Dai Dai.mp3'
+                audioUrl: 'Data/music/Shakira - Dai Dai.mp3',
+                coverUrl: 'Data/covers/7.jpg'
             },
             {
                 id: 'track-8',
                 title: 'Goals',
                 artist: 'LISA',
-                audioUrl: 'Data/music/LISA - Goals.mp3'
+                audioUrl: 'Data/music/LISA - Goals.mp3',
+                coverUrl: 'Data/covers/8.jpg'
             },
             {
                 id: 'track-9',
                 title: 'Criminal',
                 artist: 'Britney Spears',
-                audioUrl: 'Data/music/Britney Spears - Criminal.mp3'
+                audioUrl: 'Data/music/Britney Spears - Criminal.mp3',
+                coverUrl: 'Data/covers/9.jpg'
+            },
+            {
+                id: 'track-10',
+                title: 'Falling',
+                artist: 'Trevor Daniel',
+                audioUrl: 'Data/music/Trevor Daniel - Falling.mp3',
+                coverUrl: 'Data/covers/10.jpg'
             }
+            
         ];

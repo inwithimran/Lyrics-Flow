91aebf66198ea99e033db5b3e007f7afadbb1cbf
Tabib Imran <in.with.imran@gmail.com>
2026-08-17 01:22:36 +0600

    Add files via upload

--- Data/musicData.js
+++ Data/musicData.js
@@ -1,73 +1,158 @@
 window.PLAYLIST_DATA = [
-            {
-                id: 'track-1',
-                title: 'Meri Banogi Kya',
-                artist: 'Rito Riba',
-                audioUrl: 'Data/music/Rito Riba - Meri Banogi Kya.mp3',
-                coverUrl: 'Data/covers/1.jpg'
-            },
-            {
-                id: 'track-2',
-                title: 'Let Her Go',
-                artist: 'Passenger',
-                audioUrl: 'Data/music/Passenger - Let Her Go.mp3',
-                coverUrl: 'Data/covers/2.jpg'
-            },
-            {
-                id: 'track-3',
-                title: 'Juliet',
-                artist: 'Cavetown',
-                audioUrl: 'Data/music/Cavetown - Juliet.mp3',
-                coverUrl: 'Data/covers/3.jpg'
-            },
-        {
-                id: 'track-4',
-                title: 'Somewhere Only We Know',
-                artist: 'Keane',
-                audioUrl: 'Data/music/Keane - Somewhere Only We Know.mp3',
-                coverUrl: 'Data/covers/4.jpg'
-            },
-        {
-                id: 'track-5',
-                title: 'Attention',
-                artist: 'Charlie Puth',
-                audioUrl: 'Data/music/Charlie Puth - Attention.mp3',
-                coverUrl: 'Data/covers/5.jpg'
-            },
-        {
-                id: 'track-6',
-                title: 'Mera Pyar Tera Pyar',
-                artist: 'Jeet Gannguli',
-                audioUrl: 'Data/music/Jeet Gannguli - Mera Pyar Tera Pyar.mp3',
-                coverUrl: 'Data/covers/6.jpg'
-            },
-            {
-                id: 'track-7',
-                title: 'Dai Dai',
-                artist: 'Shakira',
-                audioUrl: 'Data/music/Shakira - Dai Dai.mp3',
-                coverUrl: 'Data/covers/7.jpg'
-            },
-            {
-                id: 'track-8',
-                title: 'Goals',
-                artist: 'LISA',
-                audioUrl: 'Data/music/LISA - Goals.mp3',
-                coverUrl: 'Data/covers/8.jpg'
-            },
-            {
-                id: 'track-9',
-                title: 'Criminal',
-                artist: 'Britney Spears',
-                audioUrl: 'Data/music/Britney Spears - Criminal.mp3',
-                coverUrl: 'Data/covers/9.jpg'
-            },
-            {
-                id: 'track-10',
-                title: 'Falling',
-                artist: 'Trevor Daniel',
-                audioUrl: 'Data/music/Trevor Daniel - Falling.mp3',
-                coverUrl: 'Data/covers/10.jpg'
-            }
-            
-        ];
+    {
+        id: 'track-1',
+        title: 'Faded',
+        artist: 'Alan Walker',
+        audioUrl: 'Data/music/Alan Walker - Faded.mp3'
+    },
+    {
+        id: 'track-2',
+        title: 'Let Me Down',
+        artist: 'Alec Benjamin',
+        audioUrl: 'Data/music/Alec Benjamin - Let Me Down.mp3'
+    },
+    {
+        id: 'track-3',
+        title: 'Criminal',
+        artist: 'Britney Spears',
+        audioUrl: 'Data/music/Britney Spears - Criminal.mp3'
+    },
+    {
+        id: 'track-4',
+        title: 'On & On',
+        artist: 'Cartoon, Jéja',
+        audioUrl: 'Data/music/Cartoon, Jéja - On & On.mp3'
+    },
+    {
+        id: 'track-5',
+        title: 'Juliet',
+        artist: 'Cavetown',
+        audioUrl: 'Data/music/Cavetown - Juliet.mp3'
+    },
+    {
+        id: 'track-6',
+        title: 'Attention',
+        artist: 'Charlie Puth',
+        audioUrl: 'Data/music/Charlie Puth - Attention.mp3'
+    },
+    {
+        id: 'track-7',
+        title: 'We Made It',
+        artist: 'Circa Waves',
+        audioUrl: 'Data/music/Circa Waves - We Made It.mp3'
+    },
+    {
+        id: 'track-8',
+        title: 'Arcade',
+        artist: 'Duncan Laurence',
+        audioUrl: 'Data/music/Duncan Laurence - Arcade.mp3'
+    },
+    {
+        id: 'track-9',
+        title: 'Believer',
+        artist: 'Imagine Dragons',
+        audioUrl: 'Data/music/Imagine Dragons - Believer.mp3'
+    },
+    {
+        id: 'track-10',
+        title: 'Bones',
+        artist: 'Imagine Dragons',
+        audioUrl: 'Data/music/Imagine Dragons - Bones.mp3'
+    },
+    {
+        id: 'track-11',
+        title: 'Mera Pyar Tera Pyar',
+        artist: 'Jeet Gannguli',
+        audioUrl: 'Data/music/Jeet Gannguli - Mera Pyar Tera Pyar.mp3'
+    },
+    {
+        id: 'track-12',
+        title: 'Want Me',
+        artist: 'Jimmy Hardwind',
+        audioUrl: 'Data/music/Jimmy Hardwind - Want Me.mp3'
+    },
+    {
+        id: 'track-13',
+        title: 'Home',
+        artist: 'Jordan Schor, Harley Bird',
+        audioUrl: 'Data/music/Jordan Schor, Harley Bird - Home.mp3'
+    },
+    {
+        id: 'track-14',
+        title: 'Baby',
+        artist: 'Justin Bieber',
+        audioUrl: 'Data/music/Justin Bieber - Baby.mp3'
+    },
+    {
+        id: 'track-15',
+        title: 'Somewhere Only We Know',
+        artist: 'Keane',
+        audioUrl: 'Data/music/Keane - Somewhere Only We Know.mp3'
+    },
+    {
+        id: 'track-16',
+        title: 'Goals',
+        artist: 'LISA',
+        audioUrl: 'Data/music/LISA - Goals.mp3'
+    },
+    {
+        id: 'track-17',
+        title: 'Die With A Smile',
+        artist: 'Lady Gaga, Bruno Mars',
+        audioUrl: 'Data/music/Lady Gaga, Bruno Mars - Die With A Smile.mp3'
+    },
+    {
+        id: 'track-18',
+        title: 'Best of Me',
+        artist: 'NEFFEX',
+        audioUrl: 'Data/music/NEFFEX - Best of Me.mp3'
+    },
+    {
+        id: 'track-19',
+        title: 'Let Her Go',
+        artist: 'Passenger',
+        audioUrl: 'Data/music/Passenger - Let Her Go.mp3'
+    },
+    {
+        id: 'track-20',
+        title: 'Детство',
+        artist: 'Rauf & Faik',
+        audioUrl: 'Data/music/Rauf & Faik - Детство.mp3'
+    },
+    {
+        id: 'track-21',
+        title: 'Meri Banogi Kya',
+        artist: 'Rito Riba',
+        audioUrl: 'Data/music/Rito Riba - Meri Banogi Kya.mp3'
+    },
+    {
+        id: 'track-22',
+        title: 'Dai Dai',
+        artist: 'Shakira',
+        audioUrl: 'Data/music/Shakira - Dai Dai.mp3'
+    },
+    {
+        id: 'track-23',
+        title: 'Señorita',
+        artist: 'Shawn Mendes, Camila Cabello',
+        audioUrl: 'Data/music/Shawn Mendes, Camila Cabello - Señorita.mp3'
+    },
+    {
+        id: 'track-24',
+        title: 'Falling',
+        artist: 'Trevor Daniel',
+        audioUrl: 'Data/music/Trevor Daniel - Falling.mp3'
+    },
+    {
+        id: 'track-25',
+        title: 'See You Again',
+        artist: 'Wiz Khalifa',
+        audioUrl: 'Data/music/Wiz Khalifa - See You Again.mp3'
+    },
+    {
+        id: 'track-26',
+        title: 'Past Lives',
+        artist: 'sapientdream',
+        audioUrl: 'Data/music/sapientdream - Pastlives.mp3'
+    }
+];
